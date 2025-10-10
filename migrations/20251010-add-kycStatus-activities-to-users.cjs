e78cebcc1ee4f223a4f950c6417269318cc7584e
Isreal5161 <diatecktechnology@gmail.com>
2025-10-10 19:45:03 +0100

    Remove duplicate kycStatus column from migration

--- migrations/20251010-add-kycStatus-activities-to-users.cjs
+++ migrations/20251010-add-kycStatus-activities-to-users.cjs
@@ -1,10 +1,5 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('users', 'kycStatus', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: 'unverified'
-    });
     await queryInterface.addColumn('users', 'activities', {
       type: Sequelize.JSON,
       allowNull: false,
@@ -12,7 +7,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('users', 'kycStatus');
     await queryInterface.removeColumn('users', 'activities');
   }
 };
