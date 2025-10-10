9789e1468ecba5282d70aa6235f16b2f347fdf39
Isreal5161 <diatecktechnology@gmail.com>
2025-10-10 19:57:14 +0100

    Remove duplicate activities column from migration

--- migrations/20251010-add-kycStatus-activities-to-users.cjs
+++ migrations/20251010-add-kycStatus-activities-to-users.cjs
@@ -1,12 +1,8 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('users', 'activities', {
-      type: Sequelize.JSON,
-      allowNull: false,
-      defaultValue: []
-    });
+    // No columns to add; already handled by previous migration
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('users', 'activities');
+    // No columns to remove; already handled by previous migration
   }
 };
