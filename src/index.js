697c97617e3a3a78aeeaeff5c0a6761d20b1653c
Isreal5161 <diatecktechnology@gmail.com>
2025-10-18 17:04:45 +0100

    Frontend: JWT and CORS fixes

--- src/index.js
+++ src/index.js
@@ -14,8 +14,16 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://crypto-forex-three.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
 app.use(express.json());
 
 // Debug: Log CORS headers for every response
